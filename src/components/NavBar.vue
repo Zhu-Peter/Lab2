395f240d78826978e47b0010d3d441ed30ad0d27
Zhu-Peter <gzhu@ualberta.ca>
2024-05-22 20:31:34 -0600

    added login function

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,78 +4,101 @@
             <div class="nav_button" v-for="(page, index) in links" :key="`page-${index}`">
                 <router-link :to="page.path">{{ page.name }}</router-link>
             </div>
-            <div v-if="!isLoggedIn" class="nav_button" id="login_button" >
-                <router-link to="/login">Log In</router-link>
+            <div class="nav_button" style="margin-left: auto;">
+                <router-link to="/login">
+                    <div v-if="!isLoggedIn" id="login_button">
+                        Log In
+                    </div>
+                    <div id="welcome" v-if="isLoggedIn"><span><img :src="userData.image_url" width="30px"
+                                style="border-radius: 50%; margin: 5px" alt=""></span> {{ userData.username }} ({{
+                                    userData.first_name }} {{ userData.last_name }})</div>
+                </router-link>
             </div>
-            <div v-if="isLoggedIn">Welcome !!USER!!</div>
         </nav>
     </div>
 </template>
 
 <script>
 import Cookies from 'vue-cookies';
-    export default {
-        name: 'NavBar',
-        data() {
-            return {
-                links: [
-                    {
-                        name: 'Home',
-                        path: '/'
-                    },
-                    {
-                        name: 'Menu',
-                        path: '/menu'
-                    },
-                    {
-                        name: 'Order',
-                        path: '/order'
-                    },
-                ],
-                isLoggedIn: false,
-            }
-        },
-        beforeMount() {
-            let token = Cookies.get('token');
-            console.log(token)
+import axios from 'axios';
+export default {
+    name: 'NavBar',
+    data() {
+        return {
+            links: [
+                {
+                    name: 'Home',
+                    path: '/'
+                },
+                {
+                    name: 'Menu',
+                    path: '/menu'
+                },
+                {
+                    name: 'Order',
+                    path: '/order'
+                },
+            ],
+            isLoggedIn: false,
+            userData: {
+                username: ''
+            },
+        }
+    },
+    beforeMount() {
+        let data = Cookies.get('LoginData');
+        // console.log(token)
 
-            //fix null to check
-            if(!(token === null)){
-                this.isLoggedIn = true;
+        //fix null to check
+        if (!(data === null)) {
+            this.isLoggedIn = true;
+            if (Object.keys(data).length > 2) {
+                this.userData = data
+            } else {
+                axios.request({
+                    url: 'http://209.38.6.175:5000/api/client',
+                    headers: {
+                        "x-api-key": "q1LXwh"
+                    },
+                    params: data
+                }).then((response) => { console.log(response.data[0]); this.userData = {...this.userData, ...response.data[0]}; Cookies.set('LoginData', JSON.stringify(this.userData)) });
+
             }
-        },
-    }
+
+        }
+    },
+}
 </script>
 
 <style scoped>
-
-nav{
+nav {
     display: flex;
     background-color: white;
     margin-inline: 150px;
 }
 
-.nav_button{
+.nav_button {
     display: flex;
     align-items: center;
     height: 50px;
     padding-inline: 10px;
 }
 
-#login_button{
-    margin-left: auto;
-}
-
-.nav_button:hover{
+.nav_button:hover {
     background-color: lightgray;
     cursor: pointer;
 }
 
-#login_button:hover{
+#login_button:hover {
     background-color: orange;
 }
 
-a{
+#welcome {
+    display: flex;
+    align-items: center;
+}
+
+a {
     text-decoration: none;
     color: black;
 }
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <router-link to="/"> back to home </router-link>
         <div id="login_container">
             <div>
                 <label for="">Email: </label>
@@ -9,14 +10,17 @@
                 <label for="">Password: </label>
                 <input v-model="password" type="password">
             </div>
-            <button @click="post">Log In</button><br>
+            <button @click="toLogIn">Log In</button><br>
+            <p id="error_message">
+                {{ error }}
+            </p>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-// import Cookies from 'vue-cookies';
+import Cookies from 'vue-cookies';
 
 export default {
     name: 'LoginView',
@@ -30,21 +34,59 @@ export default {
     },
     methods: {
         toLogIn: function () {
+
+            this.error = '';
             axios.request({
                 method: 'post',
-                url: 'https://innotechfoodie.ml/api/client-login',
+                url: 'http://209.38.6.175:5000/api/client-login',
+                headers: {
+                    "x-api-key": "q1LXwh",
+                },
                 data: {
                     email: this.email,
                     password: this.password
                 }
 
             }).then((request) => {
-                console.log(request)
-            }).catch((error) => { console.log(error); this.error = error.response.data.error })
+                // console.log(request.data);
+                this.token = JSON.stringify(request.data);
+                Cookies.set('LoginData', this.token)
+                this.$router.push(`/`)
+
+            }).catch((error) => { console.log(error); this.error = error.response.data })
         }
     },
 
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#login_container {
+    border: solid 3px orange;
+    background-color: white;
+    width: 500px;
+    margin-top: 200px;
+    margin-inline: auto;
+    padding: 50px 120px 50px 20px;
+
+    display: grid;
+    justify-items: end;
+
+}
+
+#login_container div {
+    margin: 20px;
+}
+
+#login_container button {
+    margin-right: 20px;
+}
+
+#login_container p {
+    height: 1em;
+}
+
+#error_message {
+    color: red;
+}
+</style>
