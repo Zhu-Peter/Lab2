c94cd4899d7c52bbd8cebd282649e657154f7e2f
Zhu-Peter <gzhu@ualberta.ca>
2024-06-13 22:34:56 -0600

    removed useless code

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -94,58 +94,8 @@ export default {
 
     },
     mounted() {
-        let old = this.raworders;
-        let orders = [];
-        let index = 0;
-        if (this.raworders !== '') {
-            //     this.orders[0].order_id = this.raworders[0].order_id
-            //     this.orders[0].items = [{id: this.raworders[0].menu_id, name: this.raworders[0].name, amount: 1, price: this.raworders[0].price}]
-
-
-            //     if(this.raworders[1].order_id !== this.raworders[0].order_id){
-            //         index = 1;
-            //     }
-
-            //     for (let i = 1; i < this.raworders.length; i++) {
-            //         do {
-            //             this.orders[index].order_id = this.raworders[i].order_id;
-
-            //         } while (this.raworders[i - 1].order_id == this.raworders[i].order_id)
-            //     }
-            let order_ids = []
-            let menu_ids = []
-            let price = 0;
-            // let amount = 0;
-            let counter = 0;
-
-            for (let i = 0; i < old.length; i++) {
-                if (!(order_ids.includes(old[i].order_id))) {
-                    order_ids.push(old[i].order_id)
-
-                    orders[index].order_id = old[i].order_id
-                    index++
-                }
-
-                if (orders[index - 1].items == undefined) { orders[index - 1].items = [] }
-
-                if (!(menu_ids.includes(old[i].menu_id))) {
-                    menu_ids.push(old[i].menu_id)
-                    counter = orders[index - 1].items.push({
-                        name: old[i].name,
-                        menu_id: old[i].menu_id,
-                        amount: 1,
-                        price: old[i].price,
-                    })
-                } else {
-                    orders[index - 1].items[counter].amount++
-                    orders[index - 1].items[counter].price = price + old[i].price
-
-                }
-            }
-            console.log(orders)
-
-
-        }
+        
+        
 
     },
 }
